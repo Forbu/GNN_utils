f4a1f1770c7418cc301065e35ad9e165349dd5a8
Adrien B <forbu14@gmail.com>
2023-03-11 15:07:45 +0000

    improving readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # GNN_utils
-GNN global architecture
+
+This repository contains GNN (graph neural network) architecture. Those are full GNN architecture.
+We have 2 kind of architecture :
+    - MeshGraphNets use in https://paperswithcode.com/method/meshgraphnet
+    - We also adapt this architecture using GAT (graph attention Network https://arxiv.org/abs/1710.10903)
+
+Those architecture are "full" architecture and does not contains only GNN layers.
--- setup.py
+++ setup.py
@@ -1,18 +1,13 @@
-
-# Description: setup.py script for the py2exe distribution of the
-#              PyTorch Geometric library.
-
+""" Description: setup.py for GNN_utils package
+"""
 import setuptools
 
 # create a setuptools setup that will name the package torch_geometric
 # and will include all the files in the torch_geometric directory
 # and that will have a dependency on torch and torch_scatter
 setuptools.setup(
-    name="torch_geometric",
+    name="gnn_utils",
     version="1.6.3",
     packages=setuptools.find_packages(),
     install_requires=["torch", "torch_scatter"],
 )
-    
-
-       
